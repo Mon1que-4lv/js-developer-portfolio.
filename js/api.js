1af52ccc12895a0b27e53939ea374ff5fb735662
Monique Alves <moniquealv27@gmail.com>
2025-04-09 14:43:33 -0300

    adicionando json

--- js/api.js
+++ js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/Mon1que-4lv/js-developer-portfolio/refs/heads/patch-1/data/profile.json'
+    const url = ''
     const fetching = await fetch (url)
     return await fetching.json()
 }
