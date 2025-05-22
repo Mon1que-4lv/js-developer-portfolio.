f2b4ae3ed0be40b149244a6b44429a1ea0e08907
Monique Alves <moniquealv27@gmail.com>
2025-05-22 11:39:17 -0300

    atualizando url foto/json

--- js/api.js
+++ js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/Mon1que-4lv/js-developer-portfolio/refs/heads/teste/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/Mon1que-4lv/js-developer-portfolio./refs/heads/main/data/profile.json'
     const fetching = await fetch (url)
     return await fetching.json()
 }
